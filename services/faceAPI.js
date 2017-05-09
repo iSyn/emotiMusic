4968878132ceee5bfbcfe2f9345c05589e4b287a
Synclair Wang <synclairwang11@yahoo.com>
2017-05-08 23:30:02 -0400

    added more styling and env

--- services/faceAPI.js
+++ services/faceAPI.js
@@ -1,9 +1,7 @@
 const fetch = require('node-fetch')
 
 const API_URL = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize'
-const API_KEY = '9955adc574574c438dab87cab4b776c5'
-
-let pic = 'https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/10847823_317412908466200_8932690225636238172_n.jpg?oh=2f0988c6e752f135fa9642f05c55bd4e&oe=59802362'
+const API_KEY = process.env.MICROSOFT_API_KEY
 
 let fetchEmotions = function(req, res, next) {
 
@@ -23,6 +21,7 @@ let fetchEmotions = function(req, res, next) {
     let body = JSON.parse(PARAMS.body)
     res.faceData = data
     res.faceData.imageLink = body.url
+    console.log('DATA', data)
     next()
   }).catch((err) => {
     console.log('ERRERERERER', err)
@@ -33,25 +32,5 @@ module.exports = {
   fetchEmotions
 }
 
-// [
-//   {
-//     faceRectangle: {
-//       height: 116,
-//       left: 150,
-//       top: 150,
-//       width: 116
-//     },
-//     scores: {
-//       anger: 0.0000046557775,
-//       contempt: 0.00002102154,
-//       disgust: 0.00002325293,
-//       fear: 0.0000013821799,
-//       happiness: 9.6941406e-8,
-//       neutral: 0.00939752,
-//       sadness: 0.9905514,
-//       surprise: 6.704521e-7
-//     }
-//   },
-//   imageLink: 'http://i.imgur.com/hxVx9.jpg'
-// ]
+
 
