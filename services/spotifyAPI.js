b46f89c54dc8a68df5a7fbfc3ff5c3c28cfa9424
Synclair Wang <synclairwang11@yahoo.com>
2017-05-13 23:33:21 -0400

    things are working for now but no spotify...

--- services/spotifyAPI.js
+++ services/spotifyAPI.js
@@ -32,13 +32,3 @@ module.exports = {
   getPlaylist,
   authorize
 }
-
-// method: "POST",
-//     url: "https://accounts.spotify.com/api/token",
-//     data: {
-//       "grant_type":    "authorization_code",
-//       "code":          code,
-//       "redirect_uri":  myurl,
-//       "client_secret": mysecret,
-//       "client_id":     myid,
-//     },
